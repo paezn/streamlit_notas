620fa4c99ba986cac82c75541e287ab22683ed17
paezn <npaez@colegioguanenta.edu.co>
2024-08-29 11:01:52 -0500

    Se agregaron metricas y filtros

--- dashboard.py
+++ dashboard.py
@@ -5,37 +5,32 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-st.header(':blue[Colegio San José de Guanentá]')
+st.header(':blue[Colegio San José de Guanentá]:school:')
 st.code("Especialidad en Sistemas", language="python")
-st.title('Dashboard Academy :school:')
+st.title(':green[Dashboard Academy]')
 
 
-# crear dataframe con notas de nivel basica, alojados en github
-df_notas_basica = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_basica_secundaria.csv')
-
-# crear dataframe con notas de nivel media, alojados en github
-df_notas_media = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_media_tecnica.csv')
-
-# limpieza de los dataframes
-df_notas_basica = df_notas_basica.astype({'codigo':'str'})
-st.write(df_notas_basica.dtypes)
+# creación de dataframes vacios
+df_notas_media = pd.DataFrame()
+df_notas_basica = pd.DataFrame()
 
 ####################################
 ## Creacion sidebar y filtros
 ####################################
 
 # Crear sidebar y poner imagen
-st.sidebar.image('escudoGuanenta.png',)
+st.sidebar.image('escudoGuanenta.png')
 
 # Agregar título a sidebar
 st.sidebar.title('Filtros')
 
 # lista niveles
-niveles = ['Básica', 'Media']
-niveles = st.sidebar.multiselect('Nivel', niveles)
+niveles = ['Todos','Básica', 'Media']
+niveles = st.sidebar.selectbox('Nivel', niveles)
 
 # lista jornadas
-jornadas = sorted(list(df_notas_basica['jornada'].unique()))
+jornadas = ['Mañana' , 'Tarde']
+#jornadas = sorted(list(df_notas_basica['jornada'].unique()))
 jornadas = st.sidebar.multiselect('Jornada', jornadas)
 
 # lista grados
@@ -43,13 +38,100 @@ grados = ['6','7','8','9','10','11']
 grados = st.sidebar.multiselect('Grado', grados)
 
 # lista grupos
-grupos1 = sorted(list(df_notas_basica['grupo'].unique()))
-grupos2 = sorted(list(df_notas_media['grupo'].unique()))
-grupos = grupos1 + grupos2
-grupos = st.sidebar.multiselect('Grupo', grupos)
+#grupos1 = sorted(list(df_notas_basica['grupo'].unique()))
+#grupos2 = sorted(list(df_notas_media['grupo'].unique()))
+#grupos = grupos1 + grupos2
+#grupos = st.sidebar.multiselect('Grupo', grupos)
 
 # lista asignaturas
-asignaturas = ['Biología','Sociales', 'Cátedra Paz', 'Artística', 'Etica', 'Religión','Educación Física', 'Castellano', 'Inglés', 'Matemáticas', 'Geometría','Estadística', 'Tecnología', 'Informática', 'Dibujo Técnico', 'Filosofía','Física', 'Química','Ciencias Políticas']
-asignaturas = sorted(asignaturas)
-asignaturas = st.sidebar.multiselect('Asignaturas', asignaturas)
+#asignaturas = ['Biología','Sociales', 'Cátedra Paz', 'Artística', 'Etica', 'Religión','Educación Física', 'Castellano', 'Inglés', 'Matemáticas', 'Geometría','Estadística', 'Tecnología', 'Informática', 'Dibujo Técnico', 'Filosofía','Física', 'Química','Ciencias Políticas']
+#asignaturas = sorted(asignaturas)
+#asignaturas = st.sidebar.multiselect('Asignaturas', asignaturas)
 
+## aplicacion de filtros de la sidebar
+
+# Nivel - Al seleccionar el nivel se crean los dataframes respectivos
+if niveles == 'Todos':
+    # crear dataframe con notas de nivel basica, alojados en github
+    df_notas_basica = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_basica_secundaria.csv')
+
+    # crear dataframe con notas de nivel media, alojados en github
+    df_notas_media = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_media_tecnica.csv')
+
+    # limpieza de los dataframes
+    df_notas_basica = df_notas_basica.astype({'codigo':'str'})
+    df_notas_media = df_notas_media.astype({'codigo':'str'})
+
+elif niveles == 'Básica':
+    # crear dataframe con notas de nivel basica, alojados en github
+    df_notas_basica = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_basica_secundaria.csv')
+    # limpieza de los dataframes
+    df_notas_basica = df_notas_basica.astype({'codigo':'str'})
+else:
+    # crear dataframe con notas de nivel media, alojados en github
+    df_notas_media = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_media_tecnica.csv')
+    # limpieza de los dataframes
+    df_notas_media = df_notas_media.astype({'codigo':'str'})
+
+# Jornada
+if jornadas:
+    df_notas_basica = df_notas_basica[df_notas_basica['jornada'].isin(jornadas)]
+    df_notas_media = df_notas_media[df_notas_media['jornada'].isin(jornadas)]
+
+# Grado
+if grados:
+    grupos_basica = sorted(list(df_notas_basica['grupo'].unique()))
+    lista_grupos_basica = []
+    for i in grupos_basica:
+        if (i.split('-')[0] in grados):
+            lista_grupos_basica.append(i)
+    df_notas_basica = df_notas_basica[df_notas_basica['grupo'].isin(lista_grupos_basica)]
+    st.dataframe(df_notas_basica)
+
+    grupos_media = sorted(list(df_notas_media['grupo'].unique()))
+    lista_grupos_media = []
+    for i in grupos_media:
+        if (i.split('-')[0] in grados):
+            lista_grupos_media.append(i)
+    df_notas_media = df_notas_media[df_notas_media['grupo'].isin(lista_grupos_media)]
+    st.dataframe(df_notas_media)
+
+
+##################################
+## Pestañas visualización gráficas
+##################################
+
+tab1, tab2, tab3 = st.tabs(['Asignaturas', 'Estudiantes', 'Grupos'])
+
+with tab1:
+    # graficas asignaturas
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Top 5 materias con mas alto promedio
+        pass
+    with col2:
+        # Top 5 materias con mas mortalidad
+        pass
+
+with tab2:
+    # graficas estudiantes
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Top 5 estudiantes con mas alto promedio
+        pass
+    with col2:
+        # Top 5 estudiantes con mas materias reprobadas
+        pass
+
+with tab3:
+    # graficas grupos
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Top 5 grupos con mas alto promedio
+        pass
+    with col2:
+        # Top 5 grupos con mas mortalidad
+        pass
--- pages/notas_en_bruto.py
+++ pages/notas_en_bruto.py
@@ -2,12 +2,13 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 import warnings
+import math
 
 warnings.filterwarnings('ignore')
 
-st.header('Colegio San José de Guanentá')
+st.header('Colegio San José de Guanentá:school:')
 st.code("Especialidad en Sistemas", language="python")
-st.title("Dashboard Academy :school:")
+st.title(":green[Dashboard Academy]")
 
 
 # crear dataframe con notas de nivel basica, alojados en github
@@ -16,13 +17,82 @@ df_notas_basica = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit
 # crear dataframe con notas de nivel media, alojados en github
 df_notas_media = pd.read_csv('https://raw.githubusercontent.com/paezn/streamlit_notas/main/notas_periodo_II_media_tecnica.csv')
 
-# mostrar dataframes
-st.header('Notas Periodo II 2024 - Básica Secundaria')
+# limpieza dataframes
+df_notas_basica = df_notas_basica.drop(columns=['periodo'])
 df_notas_basica = df_notas_basica.astype({'codigo':'str'})
+df_notas_basica.set_index('codigo', inplace=True)
+df_notas_basica = df_notas_basica.sort_values('grupo',ascending=True)
+
+df_notas_media = df_notas_media.drop(columns=['periodo'])
+df_notas_media = df_notas_media.astype({'codigo':'str'})
+df_notas_media.set_index('codigo', inplace=True)
+
+# Crear sidebar y poner imagen
+st.sidebar.image('escudoGuanenta.png')
+
+# mostrar dataframes
+st.header(':blue[Notas Periodo II 2024 - Básica Secundaria]')
+
+## Metricas basica
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    # Numero de estudiantes total
+    st.metric('Número estudiantes', df_notas_basica.shape[0])
+    # Número estudiantes jornada mañana
+    st.metric('Número estudiantes J. Mañana', df_notas_basica[df_notas_basica['jornada'] == 'Mañana'].shape[0])
+    # Número estudiantes jornada mañana
+    st.metric('Número estudiantes J. Tarde', df_notas_basica[df_notas_basica['jornada'] == 'Tarde'].shape[0])
+with col2:
+    # promedio general basica
+    st.metric('Promedio general',math.trunc(df_notas_basica['promedio'].mean()),delta_color="normal")
+    # promedio general basica j. mañana
+    st.metric('Promedio general J. Mañana',math.trunc(df_notas_basica[df_notas_basica['jornada'] == 'Mañana']['promedio'].mean()),delta_color="normal")
+    # promedio general basica j. tarde
+    st.metric('Promedio general J. Tarde',math.trunc(df_notas_basica[df_notas_basica['jornada'] == 'Tarde']['promedio'].mean()),delta_color="normal")
+
+with col3:
+    # promedio general basica
+    st.metric('Desviación estándar',math.trunc(df_notas_basica['promedio'].std()),delta_color="normal")
+    # Desviacion estandar j. mañana
+    st.metric('Desviación estándar J. Mañana',math.trunc(df_notas_basica[df_notas_basica['jornada'] == 'Mañana']['promedio'].std()),delta_color="inverse")
+    # Desviación estándar j. tarde
+    st.metric('Desviación estándar J. Tarde',math.trunc(df_notas_basica[df_notas_basica['jornada'] == 'Tarde']['promedio'].std()),delta_color="normal")
+
 st.dataframe(df_notas_basica)
 
 
-st.header('Notas Periodo II 2024 - Media Técnica')
+st.header(':blue[Notas Periodo II 2024 - Media Técnica]')
+
+# Metricas media
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    # Numero de estudiantes
+    st.metric('Número estudiantes', df_notas_media.shape[0])
+    # Número estudiantes jornada mañana
+    st.metric('Número estudiantes J. Mañana', df_notas_media[df_notas_media['jornada'] == 'Mañana'].shape[0])
+    # Número estudiantes jornada mañana
+    st.metric('Número estudiantes J. Tarde', df_notas_media[df_notas_media['jornada'] == 'Tarde'].shape[0])
+
+with col2:
+    # promedio general basica
+    st.metric('Promedio general',math.trunc(df_notas_media['promedio'].mean()),delta_color="normal")
+    # promedio general basica j. mañana
+    st.metric('Promedio general J. Mañana',math.trunc(df_notas_media[df_notas_media['jornada'] == 'Mañana']['promedio'].mean()),delta_color="normal")
+    # promedio general basica j. tarde
+    st.metric('Promedio general J. Tarde',math.trunc(df_notas_media[df_notas_media['jornada'] == 'Tarde']['promedio'].mean()),delta_color="normal")
+
+with col3:
+    # promedio general basica
+    st.metric('Desviación estándar',math.trunc(df_notas_media['promedio'].std()),delta_color="normal")
+    # Desviacion estandar j. mañana
+    st.metric('Desviación estándar J. Mañana',math.trunc(df_notas_media[df_notas_media['jornada'] == 'Mañana']['promedio'].std()),delta_color="inverse")
+    # Desviación estándar j. tarde
+    st.metric('Desviación estándar J. Tarde',math.trunc(df_notas_media[df_notas_media['jornada'] == 'Tarde']['promedio'].std()),delta_color="normal")
+
 st.dataframe(df_notas_media)
 
 
